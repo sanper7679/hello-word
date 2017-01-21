3f208274a6e90a801bdf1a041fe1a584371eec2a
sanper7679 <sanper7679@gmail.com>
2017-01-21 23:53:52 +0100

    b 1

--- TestGit/TestGit/Progra.cs
+++ TestGit/TestGit/Progra.cs
@@ -11,7 +11,7 @@ namespace TestGit
         static void Main(string[] args)
         {
             Console.WriteLine("ciao");
-            Console.ReadLine();
+            //Console.ReadLine();
         }
     }
 }
